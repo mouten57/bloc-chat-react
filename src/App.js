6147c5212477e94d734d87e19d1d04fae8082583
Matt Outen <mattoutenphysed@gmail.com>
2018-06-15 09:57:40 -0500

    list chat rooms: COMPLETE

--- src/App.js
+++ src/App.js
@@ -1,18 +1,42 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import firebase from 'firebase';
+import RoomList from './Components/RoomList';
+
+var config = {
+  apiKey: "AIzaSyCX4BNKBh-M3Yj7MTMRGUD-ZAr0c8gtYHk",
+  authDomain: "bloc-chat-7019f.firebaseapp.com",
+  databaseURL: "https://bloc-chat-7019f.firebaseio.com",
+  projectId: "bloc-chat-7019f",
+  storageBucket: "bloc-chat-7019f.appspot.com",
+  messagingSenderId: "676667791815"
+};
+firebase.initializeApp(config);
+
+const sideNavStyle = {
+  width: '25%',
+  minWidth: '170px',
+  height: '100%',
+  backgroundColor: 'lightGrey',
+  position: 'fixed!important',
+  zIndex: '1',
+  overflow: 'auto',
+}
+const appStyle = {
+  height: '550px',
+  border: '1px dotted grey'
+}
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="App" style={appStyle}>
+        <div style={sideNavStyle} className="sidebarNav">
+          <RoomList
+            firebase = {firebase}
+          />
+        
+        </div>   
       </div>
     );
   }
---
+++ src/Components/RoomList.js
@@ -0,0 +1,50 @@
+import React, { Component } from 'react';
+
+class RoomList extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+           rooms: [], 
+        };
+        this.roomsRef = this.props.firebase.database().ref('rooms');
+        console.log(this.roomsRef);
+    }
+
+    componentDidMount() {
+        this.roomsRef.on('child_added', snapshot => {
+            //events registered using 'on' method
+            //snapshot.val recieves snapshot object (actual data)
+            const room = snapshot.val();
+            console.log(room);
+            //snapshot.key contain's object key
+            room.key = snapshot.key;
+            console.log(room.key);
+            //concat merges/adds items to array and 
+            //returns new array w/out changing existing array
+            this.setState({ rooms: this.state.rooms.concat( room ) })
+            console.log(this.state.rooms.concat( room ));
+            //array of objects. each room is an object
+            //index 0: {name: 'room1', key:'1'}
+        });
+    }
+
+    render() {
+
+        return (
+            <div>
+                <h1 className="side-nav-header">Bloc Chat</h1>
+            {
+                this.state.rooms.map( (room, index) =>    
+                <div key={index}>
+                    <p className="room-number">Room {room.key}</p>
+                </div>
+                )
+    
+            }
+            </div>
+        );
+    }
+}
+
+export default RoomList;
