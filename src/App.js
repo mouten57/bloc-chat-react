12d4aac916e02d0b40dfe827e99ca471c7c14fd7
Matt Outen <mattoutenphysed@gmail.com>
2018-11-19 13:28:22 -0600

    added env, trimmed firebase, few style changes

--- src/App.js
+++ src/App.js
@@ -1,18 +1,15 @@
 import React, { Component } from 'react';
 import styles from './App.css';
-import firebase from 'firebase';
 import RoomList from './Components/RoomList';
 import MessageList from './Components/MessageList';
 import User from './Components/User';
 
-var config = {
-  apiKey: "AIzaSyCX4BNKBh-M3Yj7MTMRGUD-ZAr0c8gtYHk",
-  authDomain: "bloc-chat-7019f.firebaseapp.com",
-  databaseURL: "https://bloc-chat-7019f.firebaseio.com",
-  projectId: "bloc-chat-7019f",
-  storageBucket: "bloc-chat-7019f.appspot.com",
-  messagingSenderId: "676667791815"
-};
+import firebase from 'firebase/app';
+import 'firebase/database';
+import 'firebase/auth';
+
+import config from './firebaseConfig';
+
 firebase.initializeApp(config);
 
 class App extends Component {
@@ -21,59 +18,63 @@ class App extends Component {
     this.state = {
       activeRoom: '',
       user: '',
-      allMessages: '',
+      allMessages: ''
     };
   }
-  
-  //takes selected room from onClick in RoomList.js (from map function)
-  selectRoom = (room) => {
-    this.setState({ activeRoom: room });
-  }
 
-  setUser = (user) => {
-    this.setState({ user: user})
+  //takes selected room from onClick in RoomList.js (from map function)
+  selectRoom = room => {
+    this.setState({ activeRoom: room });
   };
 
-  setMessages = (messages) => {
-    this.setState({allMessages: messages})
-    console.log(this.state.allMessages)
-  }
+  setUser = user => {
+    this.setState({ user: user });
+  };
 
- 
+  setMessages = messages => {
+    this.setState({ allMessages: messages });
+  };
+
   render() {
     return (
       <div className={styles.App}>
-        
-          <RoomList
-            firebase = {firebase}
-            activeRoom={this.state.activeRoom}
-            selectRoom={this.selectRoom}
-            allMessages={this.state.allMessages}
+        <RoomList
+          firebase={firebase}
+          activeRoom={this.state.activeRoom}
+          selectRoom={this.selectRoom}
+          allMessages={this.state.allMessages}
+        />
+
+        <div className={styles.currentRoomDisplay}>
+          <ul className={styles.topDisplay}>
+            <li className={styles.currentRoom}>
+              <b>Current Room</b>:{' '}
+              <p className={styles.roomTitle}>
+                {this.state.activeRoom ? this.state.activeRoom.name : ''}
+              </p>
+            </li>
+            <li className={styles.usernameDisplay}>
+              <b>Username</b>:{' '}
+              <p className={styles.userTitle}>
+                {this.state.user ? this.state.user.displayName : 'Guest'}
+              </p>
+            </li>
+          </ul>
+
+          <User
+            className={styles.user}
+            firebase={firebase}
+            setUser={this.setUser}
+            user={this.state.user}
           />
+        </div>
 
-          <div className={styles.currentRoomDisplay}>
-            
-              <ul className={styles.topDisplay}>
-                <li className={styles.currentRoom}><b>Current Room</b>: <p className={styles.roomTitle}>{this.state.activeRoom ? this.state.activeRoom.name:''}</p></li>
-                <li className={styles.usernameDisplay}><b>Username</b>: <p className={styles.userTitle}>{this.state.user ? this.state.user.displayName : "Guest" }</p></li>
-              </ul>
-
-                <User 
-                  className={styles.user}
-                  firebase = {firebase}
-                  setUser = {this.setUser}
-                  user = {this.state.user} />  
-
-            </div>
-          
-        
         <MessageList
-          firebase = {firebase}
+          firebase={firebase}
           activeRoom={this.state.activeRoom}
           user={this.state.user}
           setMessages={this.setMessages}
         />
-       
       </div>
     );
   }
--- src/Components/MessageList.js
+++ src/Components/MessageList.js
@@ -1,15 +1,14 @@
 import React, { Component } from 'react';
 import styles from './MessageList.css';
-import { isTSTypeAliasDeclaration } from 'babel-types';
 
 class MessageList extends Component {
-  constructor(props){
+  constructor(props) {
     super(props);
     this.state = {
       allMessages: [],
       roomMessages: [],
-      newMessage: '',
-    }
+      newMessage: ''
+    };
     this.messagesRef = this.props.firebase.database().ref('messages/');
   }
   //triggered when parent state changes (prop that's passed changes)
@@ -21,82 +20,91 @@ class MessageList extends Component {
       //events registered using 'on' method
       //snapshot.val recieves snapshot object (actual data)
       const message = snapshot.val();
-        // console.log(message);
-        //snapshot.key contain's object key
+      //snapshot.key contain's object key
       message.key = snapshot.key;
-      // console.log(message.key);
-      //concat merges/adds items to array and 
+      //concat merges/adds items to array and
       //returns new array w/out changing existing array
-      let messages = this.state.allMessages.concat( message );
+      let messages = this.state.allMessages.concat(message);
       //setState of allMessages on mount
       //after state update, update displayed messages and scroll to bottom
       this.props.setMessages(messages);
-      this.setState({ allMessages: messages },
+      this.setState({ allMessages: messages }, () => {
+        this.updateDisplayedMessages(this.props.activeRoom);
+        this.scrollToBottom();
+      });
+    });
+    this.messagesRef.on('child_removed', snapshot => {
+      this.setState(
+        {
+          allMessages: this.state.allMessages.filter(
+            message => message.key !== snapshot.key
+          )
+        },
         () => {
           this.updateDisplayedMessages(this.props.activeRoom);
-          this.scrollToBottom();
-          });
-      });
-      this.messagesRef.on('child_removed', snapshot  => {
-        this.setState({ allMessages: this.state.allMessages.filter( message => message.key !== snapshot.key )  }, () => {
-          this.updateDisplayedMessages( this.props.activeRoom )
-        });
-      });
-      this.scrollToBottom();
-    }
+        }
+      );
+    });
+    this.scrollToBottom();
+  }
   scrollToBottom = () => {
-    this.messagesEnd.scrollIntoView({ behavior: "smooth" });
+    this.messagesEnd.scrollIntoView({ behavior: 'smooth' });
+  };
+
+  convertTimestamp = timestamp => {
+    var d = new Date(timestamp), // Convert the passed timestamp to milliseconds
+      yyyy = d.getFullYear(),
+      mm = ('0' + (d.getMonth() + 1)).slice(-2), // Months are zero based. Add leading 0.
+      dd = ('0' + d.getDate()).slice(-2), // Add leading 0.
+      hh = d.getHours(),
+      h = hh,
+      min = ('0' + d.getMinutes()).slice(-2), // Add leading 0.
+      ampm = 'AM',
+      time;
+
+    if (hh > 12) {
+      h = hh - 12;
+      ampm = 'PM';
+    } else if (hh === 12) {
+      h = 12;
+      ampm = 'PM';
+    } else if (hh === 0) {
+      h = 12;
     }
 
-  convertTimestamp = (timestamp) => {
-      var d = new Date(timestamp),	// Convert the passed timestamp to milliseconds
-        yyyy = d.getFullYear(),
-        mm = ('0' + (d.getMonth() + 1)).slice(-2),	// Months are zero based. Add leading 0.
-        dd = ('0' + d.getDate()).slice(-2),			// Add leading 0.
-        hh = d.getHours(),
-        h = hh,
-        min = ('0' + d.getMinutes()).slice(-2),		// Add leading 0.
-        ampm = 'AM',
-        time;
-          
-      if (hh > 12) {
-        h = hh - 12;
-        ampm = 'PM';
-      } else if (hh === 12) {
-        h = 12;
-        ampm = 'PM';
-      } else if (hh == 0) {
-        h = 12;
-      }
-      
-      // ie: 2013-02-18, 8:35 AM	
-      time = mm + '/' + dd + '/' + yyyy + ', ' + h + ':' + min + ' ' + ampm;
-      return time;
-    }
-  handleSubmitNewMessage = (newMessage) => {
+    // ie: 2013-02-18, 8:35 AM
+    time = mm + '/' + dd + '/' + yyyy + ', ' + h + ':' + min + ' ' + ampm;
+    return time;
+  };
+  handleSubmitNewMessage = newMessage => {
     var submitData = {
-        content: newMessage,
-        sentAt: this.convertTimestamp(Date.now()),
-        roomId: this.props.activeRoom.key,
-        username: this.props.user ? this.props.user.displayName : "Guest"
+      content: newMessage,
+      sentAt: this.convertTimestamp(Date.now()),
+      roomId: this.props.activeRoom.key,
+      username: this.props.user ? this.props.user.displayName : 'Guest'
     };
     var newMessageKey = this.messagesRef.push().key;
     var updates = {};
     updates['/messages/' + newMessageKey] = submitData;
-    if (submitData.content.length >= 1){
-        this.setState({ newMessage: ''});
-        return this.props.firebase.database().ref().update(updates);}
-    else {alert('Message must be at least 1 character.')}
-}
-  handleMessageInput = (event) => {
+    if (submitData.content.length >= 1) {
+      this.setState({ newMessage: '' });
+      return this.props.firebase
+        .database()
+        .ref()
+        .update(updates);
+    } else {
+      alert('Message must be at least 1 character.');
+    }
+  };
+  handleMessageInput = event => {
     let newMSG = event.target.value;
-    this.setState({newMessage: newMSG})
-  }
+    this.setState({ newMessage: newMSG });
+  };
   //takes activeRoom from componentwillreceiveprops on props change(active room change)
   //also takes activeRoom from componentDidMount on room load
   //filters messages to only those that match correct room, then sets state
   //finally, scrolls to bottom
-  updateDisplayedMessages=(activeRoom)=> {
+  updateDisplayedMessages = activeRoom => {
     this.setState(
       {
         roomMessages: this.state.allMessages.filter(
@@ -105,55 +113,69 @@ class MessageList extends Component {
       },
       () => this.scrollToBottom()
     );
-  }
+  };
 
-  deleteMessage=(message)=> {
-      let array = [...this.state.roomMessages];
-      var index = array.indexOf(message);
-      array.splice(index, 1);
-      this.setState({roomMessages: array})
-      this.messagesRef.child(message.key).remove().then(() => {
-          // check the browser console to see if this works... or throws an error if it is not a promise
-          console.log(`${message.content} removed`);
-         // if it is a promise we now could call remove on the messages
-       });
-    }
-  
-  render () {
-    return(
+  deleteMessage = message => {
+    let array = [...this.state.roomMessages];
+    var index = array.indexOf(message);
+    array.splice(index, 1);
+    this.setState({ roomMessages: array });
+    this.messagesRef
+      .child(message.key)
+      .remove()
+      .then(() => {
+        // check the browser console to see if this works... or throws an error if it is not a promise
+        console.log(`${message.content} removed`);
+        // if it is a promise we now could call remove on the messages
+      });
+  };
+
+  render() {
+    return (
       <div className={styles.messageListDiv}>
         <ul className={styles.messageList}>
           {/* //map roomMessages instead of allMessages */}
-            {this.state.roomMessages.map( message => 
+          {this.state.roomMessages.map(message => (
             <li key={message.key}>
-                <div className = {styles.message}>
-                    <div className={styles.content}>{message.content}</div>
-                    <div className={styles.from}>From: {message.username}</div>
-                    <div className={styles.time}>
-                      <div className={styles.left}></div>
-                      <div className={styles.sentAt}>{message.sentAt}</div>
-                      <button 
-                      className={styles.deleteButton}
-                      onClick={() => this.deleteMessage(message)}
-                      >delete</button>
-                    
-                    </div>       
-                  
+              <div className={styles.message}>
+                <div className={styles.content}>{message.content}</div>
+                <div className={styles.from}>From: {message.username}</div>
+                <div className={styles.time}>
+                  <div className={styles.left} />
+                  <div className={styles.sentAt}>{message.sentAt}</div>
+                  <button
+                    className={styles.deleteButton}
+                    onClick={() => this.deleteMessage(message)}
+                  >
+                    delete
+                  </button>
                 </div>
+              </div>
             </li>
-          )}
+          ))}
           {/* ref can't be in a map function ?? */}
-          <div ref={(el) => (this.messagesEnd = el)}/>
-          </ul>
-          <div className={styles.messageBar}>
-            <div className={styles.formWrapper}>
-                <form className={styles.messageForm}
-                onSubmit={ (e) => { e.preventDefault(); this.handleSubmitNewMessage(this.state.newMessage) }}>
-                <input className={styles.messageInput} type="text" onChange={this.handleMessageInput} value={this.state.newMessage} placeholder="Enter a message.." />
-                <input type="submit" className={styles.msgSub}/>
-                </form>
-            </div>
+          <div ref={el => (this.messagesEnd = el)} />
+        </ul>
+        <div className={styles.messageBar}>
+          <div className={styles.formWrapper}>
+            <form
+              className={styles.messageForm}
+              onSubmit={e => {
+                e.preventDefault();
+                this.handleSubmitNewMessage(this.state.newMessage);
+              }}
+            >
+              <input
+                className={styles.messageInput}
+                type="text"
+                onChange={this.handleMessageInput}
+                value={this.state.newMessage}
+                placeholder="Enter a message.."
+              />
+              <input type="submit" className={styles.msgSub} />
+            </form>
           </div>
+        </div>
       </div>
     );
   }
